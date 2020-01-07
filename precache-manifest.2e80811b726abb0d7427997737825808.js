78e6bf015073f92254c4c414b1f3ce5e7d50627d
preeti2112 <pree.php.30@gmail.com>
2020-01-07 21:32:39 +0530

    Updates

---
+++ precache-manifest.2e80811b726abb0d7427997737825808.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/rick-and-morty/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/rick-and-morty/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/rick-and-morty/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/rick-and-morty/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "c578be119491bc2bef3f",
+    "url": "/rick-and-morty/static/js/runtime~main.6390f2d1.js"
+  },
+  {
+    "revision": "ae7ce08068c0d13f7f53",
+    "url": "/rick-and-morty/static/js/main.9235c3eb.chunk.js"
+  },
+  {
+    "revision": "39cee57d676566a68761",
+    "url": "/rick-and-morty/static/js/2.a921fed2.chunk.js"
+  },
+  {
+    "revision": "ae7ce08068c0d13f7f53",
+    "url": "/rick-and-morty/static/css/main.eaa2c98d.chunk.css"
+  },
+  {
+    "revision": "39cee57d676566a68761",
+    "url": "/rick-and-morty/static/css/2.00ed55b9.chunk.css"
+  },
+  {
+    "revision": "76e6ad0dc11a3169a78eb14b4fa640b9",
+    "url": "/rick-and-morty/index.html"
+  }
+];
